cd986590a429230eb9059516e10d10ea8cc0421a
Mohsen Imany <imany.mohsen@gmail.com>
2014-10-26 10:06:32 +0330

    Mohsen Commits

---
+++ MeshkatEnterprise.Booklet.Service.UnitTest/BookCommentTypeUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Compositional.Composer;
+using MeshkatEnterprise.Infrastructure.General;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MeshkatEnterprise.Booklet.Service.UnitTest
+{
+    [TestClass]
+    public class BookCommentTypeUnitTest
+    {
+        public BookCommentTypeUnitTest()
+        {
+            ContextUtil.Init();
+        }
+        [TestMethod]
+        public void GetCommentTypesUnitTest()
+        {
+            long bookId = 24;
+            ComponentContext context = ComponentContextBinder.Lookup();
+            var commentTypeService = context.GetComponent<IBookCommentTypeService>();
+            var result = commentTypeService.GetCommentTypes(bookId);
+            Assert.IsTrue(result.Successful && result.ReturnValue != null && result.ReturnValue.Count == 8);
+
+        }
+    }
+}
